be2ea5d68666d2cf5d19546fea0ba2582577e2b8
macbookpro <coderikhbolsheikh@gmail.com>
2023-09-16 15:18:05 +0500

    Initial commit

---
+++ lib/blocs/cart/bloc/cart_event.dart
@@ -0,0 +1,25 @@
+part of 'cart_bloc.dart';
+
+sealed class CartEvent extends Equatable {
+  final CartItem item;
+  const CartEvent({required this.item});
+
+  @override
+  List<Object> get props => [];
+}
+
+class ItemAddedEvent extends CartEvent {
+  const ItemAddedEvent({required super.item});
+}
+
+class ItemRemovedEvent extends CartEvent {
+  const ItemRemovedEvent({required super.item});
+}
+
+class ItemCountIncrement extends CartEvent {
+  const ItemCountIncrement({required super.item});
+}
+
+class ItemCountDecrement extends CartEvent {
+  const ItemCountDecrement({required super.item});
+}
